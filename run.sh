dafac3355f5e0e9dbfade3c63067310af79a6602
Alex Chan <alexchanwk@gmail.com>
2018-04-04 21:00:15 +0800

    Wait for log file

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@
 
 /home/pentaho/pentaho-server/start-pentaho.sh
 
+while [ ! -f /home/pentaho/pentaho-server/tomcat/logs/pentaho.log ]
+do
+  sleep 2
+done
 tail -f /home/pentaho/pentaho-server/tomcat/logs/pentaho.log
