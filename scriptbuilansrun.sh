a3c9947a8142c6b0f817808332175c524f1a01ab
zerocool87 <mat87000@gmail.com>
2018-07-02 16:49:14 +0200

    Final version

--- scriptbuilansrun.sh
+++ scriptbuilansrun.sh
@@ -3,4 +3,4 @@ rm -rf /mnt/pentaho-data/hsqldb /mnt/pentaho-data/Repo
 docker volume prune
 docker stop $(docker ps -a -q)
 docker build -t myserver/pentahovolume .
-docker run --rm --name monpentaho3 -v /home/zero/hsqldb:/home/pentaho/pentaho-server/data/hsqldb  -p 8080:8080  -ti myserver/pentahovolume
+docker run --rm --name monpentaho3 -v /mnt/pentaho-data/hsqldb:/home/pentaho/pentaho-server/data/hsqldb  -v /mnt/pentaho-data/repo:/home/pentaho/pentaho-server/pentaho-solutions/system/jackrabbit/repository -p 8080:8080  -ti myserver/pentahovolume
